e48bce9ab4f12c890a698bd42b1507440674ae95
後藤将 <gotosho@gotoususumunoMacBook-Air.local>
2023-03-08 11:51:17 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+import { program } from "commander";
+import * as fs from 'node:fs/promises';
+import { md2html } from "./md2html";
+
+program.option('--gfm', 'GFMを有効にする');
+program.parse(process.argv);
+const filePath = program.args[0];
+
+const cliOptions = {
+    gfm: false,
+    ...program.opts(),
+}
+
+fs.readFile(filePath, { encoding: 'utf-8' })
+    .then(file => {
+        const html = md2html(file, cliOptions);
+        console.log(html);
+    })
+    .catch(err => {
+        console.error(err.message);
+        process.exit(1);
+    });
